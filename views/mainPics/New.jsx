3f21a18141ebe2d12dfbacf281d55cfeb0cb7f8c
John Davis <johnmdavis125@gmail.com>
2020-08-24 16:44:20 -0400

    all forms styled, removed unnecessary jsx files

--- views/mainPics/New.jsx
+++ views/mainPics/New.jsx
@@ -1,16 +1,27 @@
 const React = require('react'); 
+const Layout = require('../components/Layout.jsx'); 
+const Header = require('../components/Header.jsx'); 
+
+const headerTitle = 'New Gallery Image'; 
 
 class New extends React.Component {
     render(){
         return (
+            <Layout>
+            <Header
+            title={headerTitle}
+            ></Header>
             <div>
-                <h1>New Gallery Image</h1>
-                <form action='/mainPics' method='POST'>
-                    <label for='img'>Img URL</label>
-                    <input class='img' type='text' name='img'/><br/>
-                    <input class='submitButton' type='submit' name='' value='Upload New Image'/>
+                <form style={{color: 'white'}} action='/mainPics' method='POST'>
+                    <fieldset class='form-group col-md-6'>
+                        <legend>New Image Info</legend>
+                        <label for='img'>Img URL</label>
+                        <input class='img form-control' type='text' name='img'/><br/>
+                        <input class='submitButton btn btn-primary btn-sm' type='submit' name='' value='Upload New Image'/>
+                    </fieldset>
                 </form>
             </div>
+            </Layout>
         )
     }
 }
