85eff64017e2e872568ce67008a98325fc0e0cf6
John Davis <johnmdavis125@gmail.com>
2020-08-25 16:43:00 -0400

    adjusted bootstrap grid layout on albums index pg

--- views/albums/Index.jsx
+++ views/albums/Index.jsx
@@ -21,51 +21,62 @@ class Index extends React.Component {
                 {
                     albums.map((album, i)=>{
                         return (
-                        <div class='container d-flex text-center' style={{marginTop: '20px', backgroundColor: 'black'}}>
-                            <div class='row'>
-                            <div class='col-lg'>
-                                <img style={{minWidth: '200px', maxWidth: '500px', borderRadius: '10px', border: '2px solid white'}} src={album.img}/>
-                            </div>
+                        <div class='container text-center' style={{marginTop: '20px', backgroundColor: 'black'}}>
+                                    <h2 style={{marginBottom: '20px'}}>{album.albumTitle}</h2>
+                            
+                            <div class='row d-flex align-items-center'>
+                            
+                                <div class='col-lg-4 col-md-9 col-sm-12'>
+                                    <img style={{minWidth: '200px', maxWidth: '350px', maxHeight: '350px', borderRadius: '10px', border: '2px solid white'}} src={album.img}/>
+                                </div>
+                                    
 
-                            <div class='col-lg'>
-                                <h2>{album.albumTitle}</h2>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song1title}
-                                    <audio controls class='audioPlayer'><source src={album.song1file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song2title}{album.song2length}
-                                    <audio controls class='audioPlayer'><source src={album.song2file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song3title}{album.song3length}
-                                    <audio controls class='audioPlayer'><source src={album.song3file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song4title}{album.song4length}
-                                    <audio controls class='audioPlayer'><source src={album.song4file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song5title}{album.song5length}
-                                    <audio controls class='audioPlayer'><source src={album.song5file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song6title}{album.song6length}
-                                    <audio controls class='audioPlayer'><source src={album.song6file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song7title}{album.song7length}
-                                    <audio controls class='audioPlayer'><source src={album.song7file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song8title}{album.song8length}
-                                    <audio controls class='audioPlayer'><source src={album.song8file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song9title}{album.song9length}
-                                    <audio controls class='audioPlayer'><source src={album.song9file} type='audio/mp3'></source></audio></h5>
-                                <h5 style={{margin: '0', padding: '0'}}>{album.song10title}{album.song10length}
-                                    <audio controls class='audioPlayer'><source src={album.song10file} type='audio/mp3'></source></audio></h5>
+                                <div class='col-lg-6 col-md-9 col-sm-12'>
+                                    <div class='row'>
+                                        <div class='col-md-6 col-sm-6'>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song1title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song2title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song3title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song4title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song5title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song6title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song7title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song8title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song9title}</h5>
+                                            <h5 style={{margin: '0', padding: '0 0 5px 0'}}>{album.song10title}</h5>
+                                        </div>
+                                        <div class='col-md-6 col-sm-6'>
+                                            <audio controls class='audioPlayer'><source src={album.song1file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song2file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song3file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song4file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song5file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song6file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song7file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song8file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song9file} type='audio/mp3'></source></audio>
+                                            <audio controls class='audioPlayer'><source src={album.song10file} type='audio/mp3'></source></audio>
+                                        </div>
+                                    </div>
                                 </div>
-                                </div>
+
+                            </div>
+                               
                                 <a class='btn btn-light btn-sm' style={{marginTop: '20px'}} href={`/albums/${album._id}/edit`}>Edit Album</a>
 
-                                    <form action={`/albums/${album._id}?_method=DELETE`} method='POST'>
-                                        <input class='btn btn-dark btn-sm' type='submit' value='Delete Album'/>
-                                    </form>
+                                <form action={`/albums/${album._id}?_method=DELETE`} method='POST'>
+                                    <input class='btn btn-dark btn-sm' type='submit' value='Delete Album'/>
+                                </form>
 
-                                <br/>
                                 <a class='btn btn-light btn-sm' href='/albums/new'>Add New Album!</a>
-                           </div> 
+                        </div>         
+                    
                        
                         )
                     })
                  
                 }
-
+              
             </div>
         </div>
         </Layout>
@@ -74,4 +85,3 @@ class Index extends React.Component {
 }
 
 module.exports = Index; 
-
